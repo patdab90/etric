eb3b1a40f15dcf39c0572724255f8a42af83846a
patdab90 <patdab90@gmail.com>
2014-08-05 13:10:39 +0200

    utils functions

---
+++ etriutils.R
@@ -0,0 +1,27 @@
+M <- 1E+10
+MINEPS <- 1E-10
+
+etriutils.solve <- function(solver, objectioveFunction, varTypes, lhs, dir, rhs, max){
+  obj <- L_objective(objectioveFunction)
+  roiConst <- L_constraint(lhs, dir, rhs)
+  
+  lp <- OP(objective=obj, constraints=roiConst, maximum=max, types=varTypes)
+  
+  ret <- ROI_solve(lp, solver)
+  
+  return(ret)
+}
+
+etriutils.getConstraintTypes <- function(varnames, binaryVarNames){
+  types <- matrix("C", nrow = 1, ncol=length(varnames))
+  colnames(types) <- varnames
+  types[,binaryVarNames] <- "B"
+  return(types[1,])
+}
+
+etriutils.buildObjectiveFunction <- function(varnames, objectivesVarNames){
+  row <- matrix(0, ncol=length(varnames))
+  colnames(row) <- varnames
+  row[,objectivesVarNames] <- 1
+  return(row[1,])
+}
---
+++ main.R
@@ -0,0 +1,14 @@
+library(ROI.plugin.glpk)
+library(ROI)
+
+source('etrib.R')
+
+
+solvers <- ROI_installed_solvers()
+if (!is.na(solvers['symphony'])) {
+  .solver <<- 'symphony'
+} else if (!is.na(solvers['glpk'])) {
+  .solver <<- 'glpk'
+} else {
+  stop("No ROI Symphony or GLPK plugin installed")
+}
