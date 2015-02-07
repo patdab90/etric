f559b6396cd9230c1af307ac2a9040f81bc415ca
patdab90 <patdab90@gmail.com>
2015-02-07 22:41:55 +0100

    RXMCDAextendet -get threshold fixed

--- RXMCDAextended.R
+++ RXMCDAextended.R
@@ -7,6 +7,7 @@ getCriteriaThreshold <- function(tree, criteriaIDs){
   out<-matrix(0,nrow = length(criteriaIDs),ncol = 7, dimnames = list(criteriaIDs, c("preferenceDirection","ind_a","ind_b", "pref_a","pref_b", "veto_a", "veto_b")))
   err <-c()
   if (length(criteria)>0){
+    criteria
     for (i in 1:length(criteria)){
       elements <- getNodeSet(criteria[[i]], "criterion")
       if (length(elements)>0){
@@ -25,34 +26,33 @@ getCriteriaThreshold <- function(tree, criteriaIDs){
           if(x){
             # no actvie tag found, therefore supposed to be active
             cIDs<-xmlGetAttr(elements[[j]], "id")
-            message(paste0(j, cIDs))
             if(is.element(cIDs, criteriaIDs)){
-              preferenceDirection <- getNodeSet(elements[[i]], "scale/quantitative")
+              preferenceDirection <- getNodeSet(elements[[j]], "scale/quantitative")
               out[cIDs, "preferenceDirection"] <- XML::xmlValue(preferenceDirection[[1]])
               
-              ind <-getNodeSet(elements[[i]], "thresholds/threshold[@id='ind']/linear/slope")
+              ind <-getNodeSet(elements[[j]], "thresholds/threshold[@id='ind']/linear/slope")
               ind_a <- XML::xmlValue(ind[[1]])
               out[cIDs, "ind_a"] <- sapply(parse(text=ind_a), eval) 
               
-              ind <-getNodeSet(elements[[i]], "thresholds/threshold[@id='ind']/linear/intercept")
+              ind <-getNodeSet(elements[[j]], "thresholds/threshold[@id='ind']/linear/intercept")
               ind_b <- XML::xmlValue(ind[[1]])
-              out[cIDs, "ind_b"]  <- sapply(parse(text=ind_a), eval) 
+              out[cIDs, "ind_b"]  <- sapply(parse(text=ind_b), eval) 
               
-              pref <-getNodeSet(elements[[i]], "thresholds/threshold[@id='pref']/linear/slope")
-              pref_a <- XML::xmlValue(ind[[1]])
+              pref <-getNodeSet(elements[[j]], "thresholds/threshold[@id='pref']/linear/slope")
+              pref_a <- XML::xmlValue(pref[[1]])
               out[cIDs, "pref_a"]  <- sapply(parse(text=pref_a), eval) 
               
-              pref <-getNodeSet(elements[[i]], "thresholds/threshold[@id='pref']/linear/intercept")
-              pref_b <- XML::xmlValue(ind[[1]])
-              out[cIDs, "pref_b"]  <- sapply(parse(text=pref_a), eval) 
+              pref <-getNodeSet(elements[[j]], "thresholds/threshold[@id='pref']/linear/intercept")
+              pref_b <- XML::xmlValue(pref[[1]])
+              out[cIDs, "pref_b"]  <- sapply(parse(text=pref_b), eval) 
               
-              veto <-getNodeSet(elements[[i]], "thresholds/threshold[@id='veto']/linear/slope")
-              veto_a <- XML::xmlValue(ind[[1]])
+              veto <-getNodeSet(elements[[j]], "thresholds/threshold[@id='veto']/linear/slope")
+              veto_a <- XML::xmlValue(veto[[1]])
               out[cIDs, "veto_a"]  <- sapply(parse(text=veto_a), eval) 
               
-              veto <-getNodeSet(elements[[i]], "thresholds/threshold[@id='veto']/linear/slope")
-              veto_a <- XML::xmlValue(ind[[1]])
-              out[cIDs, "veto_a"]  <- sapply(parse(text=veto_a), eval) 
+              veto <-getNodeSet(elements[[j]], "thresholds/threshold[@id='veto']/linear/intercept")
+              veto_b <- XML::xmlValue(veto[[1]])
+              out[cIDs, "veto_b"]  <- sapply(parse(text=veto_b), eval) 
             }
           }
         }
--- tests/etric_model.R
+++ tests/etric_model.R
@@ -1,13 +1,13 @@
 library(RXMCDA)
 # Get the list of arguments behind the --args option
-source("RXMCDAextended.R")
+source("../RXMCDAextended.R")
 #args <- commandArgs(trailingOnly = TRUE)
 
 #alternatives
 #performanceTale
 #klasy/kategorie
 #linearConstraints
-alternativesFile <- "aalternatives.xml"
+alternativesFile <- "alternatives.xml"
 criteriaFile <- "criteria.xml"
 performanceTableFile <- "performanceTable.xml"
 
@@ -21,17 +21,18 @@ treePerformanceTable<-xmlTreeParse(performanceTableFile,useInternalNodes=TRUE)
 if(checkXSD(treeAlternatives)){
   
 }
-if(checkXSD(tree = treeCriteria)){
+if(checkXSD(treeCriteria)){
   
 }
 if(checkXSD(treePerformanceTable)){
   
 }
 
+
 critIDs <- getCriteriaIDs(treeCriteria)
 altIDs <- getAlternativesIDs(treeAlternatives)[[1]]
 perfTable <- getPerformanceTables(treePerformanceTable,altIDs = altIDs, critIDs = critIDs)
 
-
+perfTable
 th <- getCriteriaThreshold(treeCriteria, critIDs)
 th
